a06ce5f7c1a9012a01b2dd3dbaebd821b9c8a213
Stephanie Pinion <stephpinion@gmail.com>
2017-03-22 16:15:08 -0600

    Implemented user connection to posts with tests

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show]
-  
+
   def index
   end
 
@@ -10,6 +10,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
+    @post.user_id = current_user.id
 
     if @post.save
 
