2f36efd3f5622dc52bdc3052c371fc13c875d0bc
Stephanie Pinion <stephpinion@gmail.com>
2017-03-28 20:45:09 -0600

    Implemented tests and pagge redirection for permission structure

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,9 +2,7 @@ FactoryGirl.define do
   sequence :email do |n|
     "test#{n}@example.com"
   end
-end
 
-FactoryGirl.define do
   factory :user do
     first_name 'Jon'
     last_name 'Snow'
@@ -16,6 +14,14 @@ FactoryGirl.define do
   factory :admin_user, class: "AdminUser" do
     first_name 'Admin'
     last_name 'User'
+    email { generate :email }
+    password 'asdfasdf'
+    password_confirmation 'asdfasdf'
+  end
+
+  factory :non_authorized_user, class: "User" do
+    first_name 'Non'
+    last_name 'Authorized'
     email { generate :email }
     password 'asdfasdf'
     password_confirmation 'asdfasdf'
